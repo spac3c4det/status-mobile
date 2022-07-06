4733c2e05d688c0b29abc2e53d03455d02118df7
Ibrahem Khalil <33176106+vampirekiddo@users.noreply.github.com>
2022-07-06 17:29:26 +0200

    Handle count

--- src/quo/previews/reacts.cljs
+++ src/quo/previews/reacts.cljs
@@ -6,39 +6,26 @@
             [quo.design-system.colors :as colors]
             [quo.previews.preview :as preview]))
 
-
-(def all-props (preview/list-comp [clicks        [1122 212 0]
-                                   dark?         [false true false]
-                                   neutral?      [true false true]
-                                   emoji         ["😛" "👍" "❤️"]]
-                                  {:emoji emoji
-                                   :clicks clicks
-                                   :dark?   dark?
-                                   :neutral? neutral?}))
-
 (defn render-item
   "Add your emoji as a param here"
   [{:keys [emoji clicks dark? neutral?]}] 
   (let [text-color (if dark? "white" "black")
-        clicks-positive? (pos-int? clicks)]
-    [rn/view {:style {:display "flex"
-                      :flex-direction "row"
-                      :justify-content :space-evenly 
-                      :padding-vertical 8
-                      :padding-horizontal (if (not clicks-positive?)
-                                            0
-                                            8)
-                      :background-color   (if dark?
-                                            (if neutral? "#192438" "black")
-                                            (if neutral? "#F0F2F5" "white"))
-                      
-                      :margin-top 25
-                      :border-radius 10
-                      :border-color (if dark? "white" "black")
-                      :border-width 1}}
+        clicks-positive? (pos-int? @clicks)]
+    [rn/touchable-opacity {:on-press #(swap! clicks inc)
+                           :style {:display "flex"
+                                   :flex-direction "row"
+                                   :padding-vertical 8
+                                   :padding-horizontal 8
+                                   :background-color   (if dark?
+                                                         (if neutral? "#192438" "black")
+                                                         (if neutral? "#F0F2F5" "white"))
+                                   :margin-top 25
+                                   :border-radius 10
+                                   :border-color (if dark? "white" "black")
+                                   :border-width 1}}
      [quo/text {:style {:color text-color}}
       (str emoji (if clicks-positive?
-                   (str " " clicks)
+                   (str " " @clicks)
                    ""))]]))
 
 (defn preview-reacts []
@@ -46,9 +33,21 @@
             :display "flex" 
             :flex-direction "column"
             :align-items "center"}
-   [rn/flat-list {:keyboardShouldPersistTaps :always 
-                  :data                      all-props
-                  :render-fn                 render-item
-                  :key-fn                    str}]])
+   [render-item {:emoji "😛"
+                 :clicks (reagent/atom 5)
+                 :dark? false
+                 :neutral? false}]
+   [render-item {:emoji "😛"
+                 :clicks (reagent/atom 100)
+                 :dark? true
+                 :neutral? false}]
+   [render-item {:emoji "😛"
+                 :clicks (reagent/atom 0)
+                 :dark? false
+                 :neutral? true}]
+   [render-item {:emoji "😛"
+                 :clicks (reagent/atom 9999)
+                 :dark? true
+                 :neutral? true}]])
 
 
