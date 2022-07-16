e26704b8ed7b254e3895fd676172ccf15da51640
Ibrahem Khalil <33176106+vampirekiddo@users.noreply.github.com>
2022-07-16 09:35:33 +0200

    Use flex layout instead of CSS layout
    Conflicts
    
    Clean

--- src/quo2/components/channel_avatar.cljs
+++ src/quo2/components/channel_avatar.cljs
@@ -1,6 +1,5 @@
 (ns quo2.components.channel-avatar
-  (:require [quo.design-system.colors :as colors]
-            [quo2.foundations.colors :as f-colors]
+  (:require [quo2.foundations.colors :as colors]
             [quo.react-native :as rn]
             [clojure.string :as clj-string]
             [status-im.ui.components.icons.icons :as icons]))
@@ -19,14 +18,15 @@
                             0
                             (js/parseInt y)) 
                     :border-radius (if big? 32 24)
-                    :background-color (if dark? f-colors/neutral-70
-                                          f-colors/neutral-30)}}
+                    :background-color (if dark? colors/neutral-70
+                                          colors/neutral-30)}}
    [rn/view {:style {:left (if big? 6 3)
                      :top (if big? 6 3)
                      :width 20
-                     :height 20}}
-    [rn/view {:style {:top 2.5
-                      :left 2.5}}
+                     :height 20
+                     :display :flex
+                     :justify-content :center
+                     :align-items :center}} 
      [icons/icon (keyword (str "main-icons/" icon))
       {:color "nil"
        :width 15
@@ -35,7 +35,7 @@
        [rn/view {:style {:position :absolute
                          :left (if big? 12 6)
                          :top (if big? 12 6)
-                         :background-color (if dark? f-colors/neutral-90
+                         :background-color (if dark? colors/neutral-90
                                                "white")
                          :border-radius 15
                          :padding 2}}
@@ -43,7 +43,7 @@
                       :main-icons/locked
                       :main-icons/unlocked)
          {:color (if dark?
-                   f-colors/neutral-40
-                   f-colors/neutral-50)
+                   colors/neutral-40
+                   colors/neutral-50)
           :width 16
-          :height 16}]])]]]))
+          :height 16}]])]]))
