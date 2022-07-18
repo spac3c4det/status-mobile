7de51a8319365fb4d8f6c69b659b42be13e8b89b
Ibrahem Khalil <33176106+vampirekiddo@users.noreply.github.com>
2022-07-18 19:13:09 +0200

    linting
    Clean

--- src/quo2/components/channel_avatar.cljs
+++ src/quo2/components/channel_avatar.cljs
@@ -8,12 +8,26 @@
         lock-exists? (not= :none lock-status)]
     [rn/view {:style {:width (if big? 32 24)
                       :height (if big? 32 24)
+<<<<<<< HEAD
                       :top (int x)
                       :left (int y)
                       :border-radius (if big? 32 24)
                       :background-color (if dark?
                                           colors/neutral-70
                                           colors/neutral-30)}}
+=======
+                      :top (if (or (clj-string/blank? x)
+                                   (js/isNaN x))
+                             0
+                             (js/parseInt x))
+                      :left (if (or (clj-string/blank? y)
+                                    (js/isNaN y))
+                              0
+                              (js/parseInt y))
+                      :border-radius (if big? 32 24)
+                      :background-color (if dark? colors/neutral-70
+                                            colors/neutral-30)}}
+>>>>>>> eb1d51a26 (linting)
      [rn/view {:style {:left (if big? 6 3)
                        :top (if big? 6 3)
                        :width 20
@@ -21,12 +35,17 @@
                        :display :flex
                        :justify-content :center
                        :align-items :center}}
+<<<<<<< HEAD
       [icons/icon (keyword "main-icons" icon)
+=======
+      [icons/icon (keyword (str "main-icons/" icon))
+>>>>>>> eb1d51a26 (linting)
        {:color "nil"
         :width 15
         :height 15}]
       (when lock-exists?
         [rn/view {:style {:position :absolute
+<<<<<<< HEAD
                           :left (if big?
                                   16
                                   8)
@@ -36,6 +55,12 @@
                           :background-color (if dark?
                                               colors/neutral-90
                                               colors/white)
+=======
+                          :left (if big? 16 8)
+                          :top (if big? 16 8)
+                          :background-color (if dark? colors/neutral-90
+                                                "white")
+>>>>>>> eb1d51a26 (linting)
                           :border-radius 15
                           :padding 2}}
          [icons/icon (if locked?
@@ -45,4 +70,8 @@
                     colors/neutral-40
                     colors/neutral-50)
            :width 16
+<<<<<<< HEAD
            :height 16}]])]]))
+=======
+           :height 16}]])]]))
+>>>>>>> eb1d51a26 (linting)
