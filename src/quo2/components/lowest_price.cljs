c58fe832af7e5b37591e85a2c3f1e1b6f7210df3
ibrkhalil <vampirekid017@gmail.com>
2022-08-08 23:16:31 +0200

    Lint

--- src/quo2/components/lowest_price.cljs
+++ src/quo2/components/lowest_price.cljs
@@ -1,7 +1,6 @@
 (ns quo2.components.lowest-price
   (:require [quo.design-system.colors :as colors]
-            [quo.react-native :as rn]
-            [re-frame.core :as re-frame]))
+            [quo.react-native :as rn]))
 
 (defn lowest-price
   [{:keys [top-value
@@ -11,66 +10,65 @@
            top-value-bg-color
            bottom-value-bg-color
            bottom-value-text-color]}]
-  (let [{:keys [height width]} @(re-frame/subscribe [:dimensions/window])]
-    [rn/view {:style {:flex-direction :column
-                      :width "100%"
-                      :margin-top (int margin-top)
-                      :justify-content :center}}
-     [rn/view {:style
-               {:flex-direction :row
-                :justify-content :center
-                :align-items :center
-                :overflow :hidden}}
-      [rn/view {:style {:opacity 0.4
-                        :border-style :dotted
-                        :border-width 1
-                        :border-color (:text-01 @colors/theme)
-                        :flex-grow 1
-                        :overflow :hidden
-                        :border-radius 1}}]
-      [rn/view {:style {:align-self :flex-start
-                        :padding-horizontal 2
-                        :padding-vertical 2
-                        :flex-grow 0.25
-                        :border-radius 3
-                        :background-color top-value-bg-color}}
-       [rn/text {:style {:color top-value-text-color
-                         :text-align :center}} top-value]]
-      [rn/view {:style
-                {:opacity 0.4
-                 :border-style :dotted
-                 :border-width 1
-                 :border-color (:text-01 @colors/theme)
-                 :flex-grow 4
-                 :overflow :hidden
-                 :border-radius 1}}]]
-     [rn/view {:style
-               {:flex-direction :row
-                :justify-content :center
-                :align-items :center}}
-      [rn/view
-       {:style
-        {:opacity 0.4
-         :border-style :dotted
-         :border-width 1
-         :border-color (:text-01 @colors/theme)
-         :flex-grow 4
-         :overflow :hidden
-         :border-radius 1}}]
-      [rn/view {:style {:align-self :flex-start
-                        :padding-horizontal 2
-                        :padding-vertical 2
-                        :flex-grow 0.25
-                        :border-radius 3
-                        :background-color bottom-value-bg-color
-                        :border-top-color "black"}}
-       [rn/text {:style {:color bottom-value-text-color
-                         :text-align :center}} bottom-value]]
-      [rn/view {:style
-                {:opacity 0.4
-                 :border-style :dotted
-                 :border-width 1
-                 :border-color (:text-01 @colors/theme)
-                 :flex-grow 1
-                 :overflow :hidden
-                 :border-radius 1}}]]]))
+  [rn/view {:style {:flex-direction :column
+                    :width "100%"
+                    :margin-top (int margin-top)
+                    :justify-content :center}}
+   [rn/view {:style
+             {:flex-direction :row
+              :justify-content :center
+              :align-items :center
+              :overflow :hidden}}
+    [rn/view {:style {:opacity 0.4
+                      :border-style :dotted
+                      :border-width 1
+                      :border-color (:text-01 @colors/theme)
+                      :flex-grow 1
+                      :overflow :hidden
+                      :border-radius 1}}]
+    [rn/view {:style {:align-self :flex-start
+                      :padding-horizontal 2
+                      :padding-vertical 2
+                      :flex-grow 0.25
+                      :border-radius 3
+                      :background-color top-value-bg-color}}
+     [rn/text {:style {:color top-value-text-color
+                       :text-align :center}} top-value]]
+    [rn/view {:style
+              {:opacity 0.4
+               :border-style :dotted
+               :border-width 1
+               :border-color (:text-01 @colors/theme)
+               :flex-grow 4
+               :overflow :hidden
+               :border-radius 1}}]]
+   [rn/view {:style
+             {:flex-direction :row
+              :justify-content :center
+              :align-items :center}}
+    [rn/view
+     {:style
+      {:opacity 0.4
+       :border-style :dotted
+       :border-width 1
+       :border-color (:text-01 @colors/theme)
+       :flex-grow 4
+       :overflow :hidden
+       :border-radius 1}}]
+    [rn/view {:style {:align-self :flex-start
+                      :padding-horizontal 2
+                      :padding-vertical 2
+                      :flex-grow 0.25
+                      :border-radius 3
+                      :background-color bottom-value-bg-color
+                      :border-top-color "black"}}
+     [rn/text {:style {:color bottom-value-text-color
+                       :text-align :center}} bottom-value]]
+    [rn/view {:style
+              {:opacity 0.4
+               :border-style :dotted
+               :border-width 1
+               :border-color (:text-01 @colors/theme)
+               :flex-grow 1
+               :overflow :hidden
+               :border-radius 1}}]]])
