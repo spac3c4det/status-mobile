5de4ed19ec5e040c36b924770c65bc1e8df48534
ibrkhalil <vampirekid017@gmail.com>
2022-08-08 22:20:00 +0200

    Used flex layout

--- src/quo2/components/lowest_price.cljs
+++ src/quo2/components/lowest_price.cljs
@@ -12,43 +12,65 @@
            bottom-value-bg-color
            bottom-value-text-color]}]
   (let [{:keys [height width]} @(re-frame/subscribe [:dimensions/window])]
-    [rn/view {:style {:display :flex
-                      :flex-direction :column
+    [rn/view {:style {:flex-direction :column
                       :width "100%"
                       :margin-top (int margin-top)
                       :justify-content :center}}
-     [rn/view {:style {:overflow :hidden}}
-      [rn/view {:style {:border-width 2
+     [rn/view {:style
+               {:flex-direction :row
+                :justify-content :center
+                :align-items :center
+                :overflow :hidden}}
+      [rn/view {:style {:opacity 0.4
+                        :border-style :dotted
+                        :border-width 1
                         :border-color (:text-01 @colors/theme)
-                        :border-radius 1
-                        :margin -2
-                        :top (* 0.017 height)
-                        :bottom (* 0.01 height)
-                        :opacity 0.4
-                        :margin-bottom 0
-                        :border-style :dotted}}]
-      [rn/view {:z-index 1
-                :margin-left (* 0.10 width)
-                :align-self :flex-start
-                :padding 6
-                :border-radius 3
-                :background-color top-value-bg-color}
-       [rn/text {:style {:color top-value-text-color}} top-value]]]
-     [rn/view {:style {:overflow :hidden
-                       :margin-top 15}}
-      [rn/view {:style {:border-width 2
-                        :border-color (:text-01 @colors/theme)
-                        :border-radius 1
-                        :margin -2
-                        :top (* 0.017 height)
-                        :bottom (* 0.01 height)
-                        :opacity 0.4
-                        :margin-bottom 0
-                        :border-style :dotted}}]
-      [rn/view {:z-index 1
-                :align-self :flex-start
-                :margin-left (* 0.75 width)
-                :border-radius 3
-                :padding 6
-                :background-color bottom-value-bg-color}
-       [rn/text {:style {:color bottom-value-text-color}} bottom-value]]]]))
+                        :flex-grow 1
+                        :overflow :hidden
+                        :border-radius 1}}]
+      [rn/view {:style {:align-self :flex-start
+                        :padding-horizontal 2
+                        :padding-vertical 2
+                        :flex-grow 0.25
+                        :border-radius 3
+                        :background-color top-value-bg-color}}
+       [rn/text {:style {:color top-value-text-color
+                         :text-align :center}} top-value]]
+      [rn/view {:style
+                {:opacity 0.4
+                 :border-style :dotted
+                 :border-width 1
+                 :border-color (:text-01 @colors/theme)
+                 :flex-grow 4
+                 :overflow :hidden
+                 :border-radius 1}}]]
+     [rn/view {:style
+               {:flex-direction :row
+                :justify-content :center
+                :align-items :center}}
+      [rn/view
+       {:style
+        {:opacity 0.4
+         :border-style :dotted
+         :border-width 1
+         :border-color (:text-01 @colors/theme)
+         :flex-grow 4
+         :overflow :hidden
+         :border-radius 1}}]
+      [rn/view {:style {:align-self :flex-start
+                        :padding-horizontal 2
+                        :padding-vertical 2
+                        :flex-grow 0.25
+                        :border-radius 3
+                        :background-color bottom-value-bg-color
+                        :border-top-color "black"}}
+       [rn/text {:style {:color bottom-value-text-color
+                         :text-align :center}} bottom-value]]
+      [rn/view {:style
+                {:opacity 0.4
+                 :border-style :dotted
+                 :border-width 1
+                 :border-color (:text-01 @colors/theme)
+                 :flex-grow 1
+                 :overflow :hidden
+                 :border-radius 1}}]]]))
